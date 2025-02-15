3db59e839d9d5e32fc9693c3c858659ba99ae140
amuroi777 <muroiaki7722@gmail.com>
2025-02-15 21:46:12 +0900

    日付タイムゾーン設定

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,8 +1,20 @@
 class BoardsController < ApplicationController
 	def index
+		@boards = Board.all
 	end
 
 	def new
 		@board = Board.new
+	end
+
+	def create
+		@board = Board.create(board_params)
+		binding.pry
+	end
+
+	private
+
+	def board_params
+  params.require(:board).permit(:author_name, :title, :body)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   get 'boards', to: 'boards#index'
   get 'boards/new', to: 'boards#new'
+  post 'boards', to: 'boards#create'
 end
