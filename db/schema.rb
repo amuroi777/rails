695d93425cdf40728268200b4af08aa4d016d27c
amuroi777 <muroiaki7722@gmail.com>
2025-02-20 21:43:14 +0900

    アノテーション、アソシエーション設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_11_111416) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_20_123443) do
   create_table "boards", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "author_name"
     t.string "title"
@@ -19,4 +19,14 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_11_111416) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "board_id", null: false
+    t.string "name", null: false
+    t.text "comment", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["board_id"], name: "index_comments_on_board_id"
+  end
+
+  add_foreign_key "comments", "boards"
 end
