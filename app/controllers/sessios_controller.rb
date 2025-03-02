a6dadaa576b88370ec5e96c6e9e1f84d5165a0fc
amuroi777 <muroiaki7722@gmail.com>
2025-03-02 20:51:20 +0900

    ユーザー認証機能で使用するルーティングのファイル作成

---
+++ app/controllers/sessios_controller.rb
@@ -0,0 +1,7 @@
+class SessiosController < ApplicationController
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
+  get 'users/me'
+  get 'home/index'
+  get 'sessios/create'
+  get 'sessios/destroy'
   root 'home#index'
   resources :boards
   resources :comments, only: %i[create destroy edit update]
