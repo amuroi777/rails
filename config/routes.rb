a4ccaa648807bd7528b74d91b949d381ba292b06
amuroi777 <muroiaki7722@gmail.com>
2025-03-02 14:32:39 +0900

    ヘッダーメニューの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  # get "up" => "rails/health#show", as: :rails_health_check
-
-  # get 'boards', to: 'boards#index'
-  # get 'boards/new', to: 'boards#new'
-  # post 'boards', to: 'boards#create'
-  # get 'boards/:id', to: 'boards#show'
-
+  root 'home#index'
   resources :boards
   resources :comments, only: %i[create destroy edit update]
 end
