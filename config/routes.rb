43e9c3755e4ba2873e3688ad4326d412de67a15b
amuroi777 <muroiaki7722@gmail.com>
2025-03-08 11:29:48 +0900

    認証機能のルーティング、ユーザーモデルの認証機能設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
-  get 'users/me'
-  get 'home/index'
-  get 'sessios/create'
-  get 'sessios/destroy'
+  get 'mypage', to: 'user#me'
+  post 'login' , to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   root 'home#index'
+  
+  resources :users, only: %i[new create]
   resources :boards
   resources :comments, only: %i[create destroy edit update]
 end
